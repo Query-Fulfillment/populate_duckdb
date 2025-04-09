8becbf4a6ef1a62ed79bff216f42a3d68a501345
Qiwei Shen <shenq@chop.edu>
2025-04-09 15:55:24 -0400

    Add local parquet

---
+++ local/load_local_parquet.R
@@ -0,0 +1,24 @@
+library(DBI)
+library(duckdb)
+library(dplyr)
+
+# local path to store duckdb file
+path_to_db <- '/data/myData.duckdb'
+
+# list of cdm_tables to load to duckdb
+cdm_tables_to_load <- c('demographic', 'diagnosis')
+
+
+#####################################################################################
+con <- dbConnect(duckdb::duckdb(), dbdir = path_to_db)
+
+for (table_name in cdm_tables_to_load ) {
+  file_pattern <- sprintf('/data/%s.parquet',table_name) # change file pattern here
+  sql <- sprintf("DROP TABLE IF EXISTS %s; 
+                 CREATE TABLE %s AS 
+                 SELECT * FROM '%s';
+                 ", table_name, table_name, file_pattern)
+  print(paste0('Loading ', table_name , '...'))
+  dbExecute(con, sql)
+}
+dbDisconnect(con)
